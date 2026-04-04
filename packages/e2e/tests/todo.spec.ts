6be6e5e3e4ec4e9a6cefa7a95582a3b506618766
radekBednarik <radek.bednarik@tesena.com>
2026-04-04 08:36:08 +0200

    chore: Make tests in describe block for testing

--- packages/e2e/tests/todo.spec.ts
+++ packages/e2e/tests/todo.spec.ts
@@ -1,29 +1,31 @@
 import { expect, test } from '@playwright/test'
 
-test('homepage loads with correct title', async ({ page }) => {
-  await page.goto('/')
-  await expect(page).toHaveTitle('Demo Todo App')
-})
+test.describe('Todo App', () => {
+  test('homepage loads with correct title', async ({ page }) => {
+    await page.goto('/')
+    await expect(page).toHaveTitle('Demo Todo App')
+  })
 
-test('can add a todo item', async ({ page }) => {
-  await page.goto('/')
-  await page.fill('[data-testid="todo-input"]', 'Buy milk')
-  await page.click('[data-testid="add-button"]')
-  await expect(page.locator('[data-testid="todo-list"]')).toContainText('Buy milk')
-})
+  test('can add a todo item', async ({ page }) => {
+    await page.goto('/')
+    await page.fill('[data-testid="todo-input"]', 'Buy milk')
+    await page.click('[data-testid="add-button"]')
+    await expect(page.locator('[data-testid="todo-list"]')).toContainText('Buy milk')
+  })
 
-test('can mark a todo item as complete', async ({ page }) => {
-  await page.goto('/')
-  await page.fill('[data-testid="todo-input"]', 'Walk the dog')
-  await page.click('[data-testid="add-button"]')
-  await page.click('[data-testid="complete-button"]')
-  await expect(page.locator('[data-testid="todo-item"]')).toHaveClass(/done/)
-})
+  test('can mark a todo item as complete', async ({ page }) => {
+    await page.goto('/')
+    await page.fill('[data-testid="todo-input"]', 'Walk the dog')
+    await page.click('[data-testid="add-button"]')
+    await page.click('[data-testid="complete-button"]')
+    await expect(page.locator('[data-testid="todo-item"]')).toHaveClass(/done/)
+  })
 
-test('can delete a todo item', async ({ page }) => {
-  await page.goto('/')
-  await page.fill('[data-testid="todo-input"]', 'Take out trash')
-  await page.click('[data-testid="add-button"]')
-  await page.click('[data-testid="delete-button"]')
-  await expect(page.locator('[data-testid="todo-list"]')).not.toContainText('Take out trash')
+  test('can delete a todo item', async ({ page }) => {
+    await page.goto('/')
+    await page.fill('[data-testid="todo-input"]', 'Take out trash')
+    await page.click('[data-testid="add-button"]')
+    await page.click('[data-testid="delete-button"]')
+    await expect(page.locator('[data-testid="todo-list"]')).not.toContainText('Take out trash')
+  })
 })
